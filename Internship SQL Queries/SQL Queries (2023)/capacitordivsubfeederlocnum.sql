0da9691f75f135eb1a4884736b1cdec3812e12e9
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:12:17 -0600

    Update capacitordivsubfeederlocnum.sql

--- Internship SQL Queries/SQL Queries (2023)/capacitordivsubfeederlocnum.sql
+++ Internship SQL Queries/SQL Queries (2023)/capacitordivsubfeederlocnum.sql
@@ -1,26 +1,26 @@
 SELECT
-    decode(disgisdb.substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
+    decode(substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
            'asmrd', 'Meridian') AS division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid,
-    disgisdb.capacitor_evw.loc_num,
-    disgisdb.capacitor_evw.owned_by,
-    decode(disgisdb.capacitor_evw.voltage, '89', '23', '75', '12', '54', '4') as voltage,
-    disgisdb.capacitor_evw.location,
-    disgisdb.capacitor_evw.kvar,
-    decode(disgisdb.capacitor_evw.dagroup_globalid, NULL, 'N', 'Y') AS SCADA,
-    decode(disgisdb.capacitor_evw.ctrl, 'NOC', 'Fixed', 'Switched') as CONTROL_TYPE
+    substation_evw.substation_name,
+    getaway_evw.feederid,
+    capacitor_evw.loc_num,
+    capacitor_evw.owned_by,
+    decode(capacitor_evw.voltage, '89', '23', '75', '12', '54', '4') as voltage,
+    capacitor_evw.location,
+    capacitor_evw.kvar,
+    decode(capacitor_evw.dagroup_globalid, NULL, 'N', 'Y') AS SCADA,
+    decode(capacitor_evw.ctrl, 'NOC', 'Fixed', 'Switched') as CONTROL_TYPE
 FROM
     (
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
-    INNER JOIN disgisdb.capacitor_evw ON disgisdb.getaway_evw.feederid = disgisdb.capacitor_evw.feederid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
+    INNER JOIN capacitor_evw ON getaway_evw.feederid = capacitor_evw.feederid
 WHERE
-    disgisdb.getaway_evw.last_we = 'N' and
-    disgisdb.capacitor_evw.owned_by = 'MPC'
+    getaway_evw.last_we = 'N' and
+    capacitor_evw.owned_by = 'MPC'
 ORDER BY
     division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid
+    substation_evw.substation_name,
+    getaway_evw.feederid
