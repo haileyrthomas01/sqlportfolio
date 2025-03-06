facfead5f951255109e259de4f283c8156c496ec
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:28:33 -0600

    Update lightsizeandltypecount.sql

--- Internship SQL Queries/SQL Queries (2024)/lightsizeandltypecount.sql
+++ Internship SQL Queries/SQL Queries (2024)/lightsizeandltypecount.sql
@@ -1,9 +1,9 @@
 SELECT
     COUNT(*),
-    disgisdb.light_evw.siz   AS  "LIGHT SIZE",
-    disgisdb.light_evw.ltype AS "LUMINAIRE TYPE"
+    light_evw.siz   AS  "LIGHT SIZE",
+    light_evw.ltype AS "LUMINAIRE TYPE"
 FROM
-    disgisdb.light_evw
+    light_evw
 GROUP BY
-    disgisdb.light_evw.siz,
-    disgisdb.light_evw.ltype
+    light_evw.siz,
+    light_evw.ltype
