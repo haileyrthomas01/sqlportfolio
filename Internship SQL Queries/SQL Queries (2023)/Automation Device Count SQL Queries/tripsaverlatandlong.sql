676a880dab310aa5c41fb2d3b7849a58e0b9a212
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:11:28 -0600

    Update tripsaverlatandlong.sql

--- Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/tripsaverlatandlong.sql
+++ Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/tripsaverlatandlong.sql
@@ -1,11 +1,11 @@
 select 
-    disgisdb.recloser_evw.objectid,
-    disgisdb.recloser_evw.latitude,
-    disgisdb.recloser_evw.longitude
+    recloser_evw.objectid,
+    recloser_evw.latitude,
+    recloser_evw.longitude
 from
-    disgisdb.recloser_evw
+    recloser_evw
 where
-    disgisdb.recloser_evw.subtypecd = '544'
-    and disgisdb.recloser_evw.owned_by = 'MPC' 
-    and disgisdb.recloser_evw.facility_status = 'E'
-    and disgisdb.recloser_evw.feederid Not In ('KC','S2546','S2566','S2576')
+    recloser_evw.subtypecd = '544'
+    and recloser_evw.owned_by = 'MPC' 
+    and recloser_evw.facility_status = 'E'
+    and recloser_evw.feederid Not In ('KC','S2546','S2566','S2576')
