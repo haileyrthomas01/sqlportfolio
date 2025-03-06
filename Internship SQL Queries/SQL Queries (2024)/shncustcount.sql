d82637e8c6d8d26eb47fff6d9b17bbb2cdc3272d
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:30:55 -0600

    Update shncustcount.sql

--- Internship SQL Queries/SQL Queries (2024)/shncustcount.sql
+++ Internship SQL Queries/SQL Queries (2024)/shncustcount.sql
@@ -1,9 +1,9 @@
 SELECT
-    SUM(disgisdb.customer_count.customers) AS customers
+    SUM(customer_count.customers) AS customers
 FROM
-         disgisdb.customer_count
-    INNER JOIN disgisdb.getaway_evw ON disgisdb.customer_count.feederid = disgisdb.getaway_evw.feederid
+         customer_count
+    INNER JOIN getaway_evw ON customer_count.feederid = getaway_evw.feederid
 WHERE
-    disgisdb.customer_count.parent_ptag IS NULL
-    and disgisdb.getaway_evw.shn_name IS NOT NULL
-    and disgisdb.getaway_evw.last_we = 'N'
+    customer_count.parent_ptag IS NULL
+    and getaway_evw.shn_name IS NOT NULL
+    and getaway_evw.last_we = 'N'
