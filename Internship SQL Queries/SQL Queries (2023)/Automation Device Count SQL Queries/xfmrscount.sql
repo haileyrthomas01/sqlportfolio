27717d9fb657c98bf693efc4f775a5fce1a53b89
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:11:52 -0600

    Update xfmrscount.sql

--- Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/xfmrscount.sql
+++ Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/xfmrscount.sql
@@ -1,8 +1,8 @@
 select
-    count(disgisdb.transformer_obj_evw.objectid) as COUNT
+    count(transformer_obj_evw.objectid) as COUNT
 from
-    disgisdb.transformer_obj_evw inner join disgisdb.transformer_evw
-    on disgisdb.transformer_obj_evw.transformer_globalid = disgisdb.transformer_evw.globalid
+    transformer_obj_evw inner join transformer_evw
+    on transformer_obj_evw.transformer_globalid = transformer_evw.globalid
 where 
-    disgisdb.transformer_evw.loc_num is NULL 
-    or disgisdb.transformer_evw.loc_num <> 'TCMS DUMMY'
+    transformer_evw.loc_num is NULL 
+    or transformer_evw.loc_num <> 'TCMS DUMMY'
