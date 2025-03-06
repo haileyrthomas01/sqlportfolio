7f1920a61fb9b546fa080f3d5b718a62f8e7d482
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:06:29 -0600

    Update distributionsubs.sql

--- Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/distributionsubs.sql
+++ Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/distributionsubs.sql
@@ -1,8 +1,8 @@
 SELECT
-    count(DISTINCT disgisdb.substation_evw.substation_name) AS count
+    count(DISTINCT substation_evw.substation_name) AS count
 FROM
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
 WHERE
-    disgisdb.getaway_evw.last_we = 'N'
+    getaway_evw.last_we = 'N'
