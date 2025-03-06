bd8b13655d0fc224a85149d37cf9451bdfc17876
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:15:32 -0600

    Update poleowners.sql

--- Internship SQL Queries/SQL Queries (2023)/poleowners.sql
+++ Internship SQL Queries/SQL Queries (2023)/poleowners.sql
@@ -1,9 +1,9 @@
 SELECT
     COUNT(*),
-    disgisdb.pole_evw.owned_by AS owned_by1
+    pole_evw.owned_by AS owned_by1
 FROM
-    disgisdb.pole_evw
+    pole_evw
 GROUP BY
-    disgisdb.pole_evw.owned_by
+    pole_evw.owned_by
 ORDER BY
     owned_by1
