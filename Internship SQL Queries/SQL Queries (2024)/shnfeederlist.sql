b16f0175fc0596eae83a3b7835d5e23cea6e1603
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:31:33 -0600

    Update shnfeederlist.sql

--- Internship SQL Queries/SQL Queries (2024)/shnfeederlist.sql
+++ Internship SQL Queries/SQL Queries (2024)/shnfeederlist.sql
@@ -1,22 +1,22 @@
 SELECT
-    decode(disgisdb.substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
+    decode(substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
            'asmrd', 'Meridian') AS division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid,
-    disgisdb.customer_count.customers
+    substation_evw.substation_name,
+    getaway_evw.feederid,
+    customer_count.customers
 FROM
     (
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
-    INNER JOIN disgisdb.customer_count ON disgisdb.getaway_evw.feederid = disgisdb.customer_count.feederid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
+    INNER JOIN customer_count ON getaway_evw.feederid = customer_count.feederid
 WHERE
-    disgisdb.getaway_evw.last_we = 'N'
-    and disgisdb.customer_count.parent_ptag IS NULL
-    and disgisdb.getaway_evw.shn_name IS NOT NULL
+    getaway_evw.last_we = 'N'
+    and customer_count.parent_ptag IS NULL
+    and getaway_evw.shn_name IS NOT NULL
 ORDER BY
     division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid
+    substation_evw.substation_name,
+    getaway_evw.feederid
  
