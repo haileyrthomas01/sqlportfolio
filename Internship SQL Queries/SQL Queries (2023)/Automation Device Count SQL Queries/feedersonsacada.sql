61d7965dc0a4cb584207eb704c63f9b33da4871b
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:06:43 -0600

    Update feedersonsacada.sql

--- Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/feedersonsacada.sql
+++ Internship SQL Queries/SQL Queries (2023)/Automation Device Count SQL Queries/feedersonsacada.sql
@@ -1,22 +1,22 @@
 SELECT
-    decode(disgisdb.substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
+    decode(substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
            'asmrd', 'Meridian') AS division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid,
-    disgisdb.recloser_evw.loc_num,
-    decode(disgisdb.recloser_evw.dagroup_globalid, NULL, 'N', 'Y') AS SCADA
+    substation_evw.substation_name,
+    getaway_evw.feederid,
+    recloser_evw.loc_num,
+    decode(recloser_evw.dagroup_globalid, NULL, 'N', 'Y') AS SCADA
 FROM
     (
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
-    INNER JOIN disgisdb.recloser_evw ON disgisdb.getaway_evw.feederid = disgisdb.recloser_evw.feederid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
+    INNER JOIN recloser_evw ON getaway_evw.feederid = recloser_evw.feederid
 WHERE   
-    disgisdb.recloser_evw.subtypecd = '105' 
-    and disgisdb.recloser_evw.facility_status = 'E'
+    recloser_evw.subtypecd = '105' 
+    and recloser_evw.facility_status = 'E'
 ORDER BY
     division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid,
-    disgisdb.recloser_evw.loc_num
+    substation_evw.substation_name,
+    getaway_evw.feederid,
+    recloser_evw.loc_num
