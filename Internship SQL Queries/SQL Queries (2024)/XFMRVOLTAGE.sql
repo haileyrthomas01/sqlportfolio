1ff151e512512b883023f6d9704f1ac356333f3f
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:19:35 -0600

    Update XFMRVOLTAGE.sql

--- Internship SQL Queries/SQL Queries (2024)/XFMRVOLTAGE.sql
+++ Internship SQL Queries/SQL Queries (2024)/XFMRVOLTAGE.sql
@@ -1,19 +1,19 @@
 SELECT
-    disgisdb.transformer_obj_evw.hi_volt,
-    disgisdb.transformer_obj_evw.kva_rating,
-    COUNT(disgisdb.transformer_obj_evw.objectid) AS "XFMR COUNT"
+    transformer_obj_evw.hi_volt,
+    transformer_obj_evw.kva_rating,
+    COUNT(transformer_obj_evw.objectid) AS "XFMR COUNT"
 FROM
-    disgisdb.transformer_evw INNER JOIN disgisdb.transformer_obj_evw 
-        ON (disgisdb.transformer_evw.globalid = disgisdb.transformer_obj_evw.transformer_globalid)
+    transformer_evw INNER JOIN transformer_obj_evw 
+        ON (transformer_evw.globalid = transformer_obj_evw.transformer_globalid)
 WHERE 
-    disgisdb.transformer_obj_evw.hi_volt IN ('13.2','7.2')
-         AND disgisdb.transformer_obj_evw.type IN ('CON', 'CSP', 'UAR') 
-         AND disgisdb.transformer_evw.owned_by = 'MPC'
-         AND disgisdb.transformer_evw.facility_status = 'E'
+    transformer_obj_evw.hi_volt IN ('13.2','7.2')
+         AND transformer_obj_evw.type IN ('CON', 'CSP', 'UAR') 
+         AND transformer_evw.owned_by = 'MPC'
+         AND transformer_evw.facility_status = 'E'
 GROUP BY
-    disgisdb.transformer_obj_evw.hi_volt,
-    disgisdb.transformer_obj_evw.kva_rating
+    transformer_obj_evw.hi_volt,
+    transformer_obj_evw.kva_rating
 ORDER BY
-    disgisdb.transformer_obj_evw.hi_volt,
-    disgisdb.transformer_obj_evw.kva_rating
+    transformer_obj_evw.hi_volt,
+    transformer_obj_evw.kva_rating
     
