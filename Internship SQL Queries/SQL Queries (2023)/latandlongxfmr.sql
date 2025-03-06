6fd16795bc89d22e84718891cf07cf4b327cc73b
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:13:54 -0600

    Update latandlongxfmr.sql

--- Internship SQL Queries/SQL Queries (2023)/latandlongxfmr.sql
+++ Internship SQL Queries/SQL Queries (2023)/latandlongxfmr.sql
@@ -1,6 +1,6 @@
-select disgisdb.transformer_obj_evw.objectid, 
-disgisdb.transformer_evw.latitude, disgisdb.transformer_evw.longitude,
-disgisdb.transformer_evw.feederid, disgisdb.transformer_obj_evw.kva_rating as "SIZE", disgisdb.transformer_obj_evw.type
-from disgisdb.transformer_evw inner join disgisdb.transformer_obj_evw on
-    (disgisdb.transformer_evw.globalid = disgisdb.transformer_obj_evw.transformer_globalid)
+select transformer_obj_evw.objectid, 
+transformer_evw.latitude, transformer_evw.longitude,
+transformer_evw.feederid, transformer_obj_evw.kva_rating as "SIZE", transformer_obj_evw.type
+from transformer_evw inner join transformer_obj_evw on
+    (transformer_evw.globalid = transformer_obj_evw.transformer_globalid)
 
