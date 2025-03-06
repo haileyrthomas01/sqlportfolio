384dba8fb6219098f2f86f27762c16e91f48db7b
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:20:45 -0600

    Update automatedswitches.sql

--- Internship SQL Queries/SQL Queries (2024)/automatedswitches.sql
+++ Internship SQL Queries/SQL Queries (2024)/automatedswitches.sql
@@ -1,8 +1,8 @@
 select 
-    count(disgisdb.switch_evw.objectid) as count
+    count(switch_evw.objectid) as count
 from
-    disgisdb.switch_evw
+    switch_evw
 where
-    disgisdb.switch_evw.facility_status = 'E'
-    and disgisdb.switch_evw.dagroup_globalid is NOT NULL
-    and disgisdb.switch_evw.feederid Not In ('KC','S2546','S2566','S2576')
+    switch_evw.facility_status = 'E'
+    and switch_evw.dagroup_globalid is NOT NULL
+    and switch_evw.feederid Not In ('KC','S2546','S2566','S2576')
