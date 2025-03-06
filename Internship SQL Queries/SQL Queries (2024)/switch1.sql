7af400520d5036dc04e7b0a0c01c2afae25c065e
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:32:25 -0600

    Update switch1.sql

--- Internship SQL Queries/SQL Queries (2024)/switch1.sql
+++ Internship SQL Queries/SQL Queries (2024)/switch1.sql
@@ -1,23 +1,23 @@
 SELECT
-    decode(disgisdb.substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
+    decode(substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
            'asmrd', 'Meridian') AS division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid,
-    disgisdb.switch_evw.loc_num,
-    disgisdb.switch_evw.recloser_globalid
+    substation_evw.substation_name,
+    getaway_evw.feederid,
+    switch_evw.loc_num,
+    switch_evw.recloser_globalid
 FROM 
     (
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
-    INNER JOIN disgisdb.switch_evw ON disgisdb.getaway_evw.feederid = disgisdb.switch_evw.feederid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
+    INNER JOIN switch_evw ON getaway_evw.feederid = switch_evw.feederid
 WHERE
-    disgisdb.getaway_evw.last_we = 'N' and
-    disgisdb.switch_evw.owned_by = 'MPC' and
-    disgisdb.switch_evw.subtypecd = '126' and
-    disgisdb.switch_evw.recloser_globalid is NULL
+    getaway_evw.last_we = 'N' and
+    switch_evw.owned_by = 'MPC' and
+    switch_evw.subtypecd = '126' and
+    switch_evw.recloser_globalid is NULL
 ORDER BY
     division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid
+    substation_evw.substation_name,
+    getaway_evw.feederid
