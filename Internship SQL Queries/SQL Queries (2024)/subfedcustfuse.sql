07b7a07f49855f6aff1a4a410e58856e8504e939
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:31:55 -0600

    Update subfedcustfuse.sql

--- Internship SQL Queries/SQL Queries (2024)/subfedcustfuse.sql
+++ Internship SQL Queries/SQL Queries (2024)/subfedcustfuse.sql
@@ -1,24 +1,24 @@
 SELECT
-    decode(disgisdb.substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
+    decode(substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
            'asmrd', 'Meridian') AS division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid,
-    disgisdb.customer_count.customers,
-    disgisdb.fuse_evw.loc_num,
-    disgisdb.fuse_evw.frate,
-    disgisdb.fuse_evw.ftype
+    substation_evw.substation_name,
+    getaway_evw.feederid,
+    customer_count.customers,
+    fuse_evw.loc_num,
+    fuse_evw.frate,
+    fuse_evw.ftype
 FROM
     (
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
-    INNER JOIN disgisdb.fuse_evw ON disgisdb.getaway_evw.feederid = disgisdb.fuse_evw.feederid
-    INNER JOIN disgisdb.customer_count ON disgisdb.getaway_evw.feederid = disgisdb.customer_count.switch_loc_num
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
+    INNER JOIN fuse_evw ON getaway_evw.feederid = fuse_evw.feederid
+    INNER JOIN customer_count ON getaway_evw.feederid = customer_count.switch_loc_num
 WHERE
-    disgisdb.getaway_evw.last_we = 'N'
-    and disgisdb.fuse_evw.frate >= 100
+    getaway_evw.last_we = 'N'
+    and fuse_evw.frate >= 100
 ORDER BY
     division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid
+    substation_evw.substation_name,
+    getaway_evw.feederid
