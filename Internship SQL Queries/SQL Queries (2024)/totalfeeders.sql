9a7f1f5f4c2ad1dcecd0dd29be3a804174142460
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:32:47 -0600

    Update totalfeeders.sql

--- Internship SQL Queries/SQL Queries (2024)/totalfeeders.sql
+++ Internship SQL Queries/SQL Queries (2024)/totalfeeders.sql
@@ -1,8 +1,8 @@
 SELECT
-    count(disgisdb.customer_count.customers) AS "TOTAL FEEDERS"
+    count(customer_count.customers) AS "TOTAL FEEDERS"
 FROM
-         disgisdb.customer_count
-    INNER JOIN disgisdb.getaway_evw ON disgisdb.customer_count.feederid = disgisdb.getaway_evw.feederid
+         customer_count
+    INNER JOIN getaway_evw ON customer_count.feederid = getaway_evw.feederid
 WHERE
-    disgisdb.customer_count.parent_ptag IS NULL
-    AND disgisdb.getaway_evw.last_we = 'N'
+    customer_count.parent_ptag IS NULL
+    AND getaway_evw.last_we = 'N'
