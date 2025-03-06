c07f315c478198f19302d710fd93bcb22c260396
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:31:03 -0600

    Update shnfeedercount.sql

--- Internship SQL Queries/SQL Queries (2024)/shnfeedercount.sql
+++ Internship SQL Queries/SQL Queries (2024)/shnfeedercount.sql
@@ -1,13 +1,13 @@
 SELECT
-    count(disgisdb.customer_count.customers) as "SHN FEEDER COUNT"
+    count(customer_count.customers) as "SHN FEEDER COUNT"
 FROM
     (
-    disgisdb.getaway_evw 
-    INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+    getaway_evw 
+    INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
-    INNER JOIN disgisdb.customer_count ON disgisdb.getaway_evw.feederid = disgisdb.customer_count.feederid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
+    INNER JOIN customer_count ON getaway_evw.feederid = customer_count.feederid
 WHERE
-    disgisdb.getaway_evw.last_we = 'N'
-    and disgisdb.customer_count.parent_ptag IS NULL
-    and disgisdb.getaway_evw.shn_name IS NOT NULL
+    getaway_evw.last_we = 'N'
+    and customer_count.parent_ptag IS NULL
+    and getaway_evw.shn_name IS NOT NULL
