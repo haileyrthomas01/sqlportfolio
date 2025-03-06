d6b14cc74048b1c8033c9d51ba480467be49f344
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:32:03 -0600

    Update substationfeeder.sql

--- Internship SQL Queries/SQL Queries (2024)/substationfeeder.sql
+++ Internship SQL Queries/SQL Queries (2024)/substationfeeder.sql
@@ -1,17 +1,17 @@
 SELECT
-    decode(disgisdb.substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
+    decode(substation_evw.org_id, 'ascst', 'Coast', 'ashttb', 'Pine Belt',
            'asmrd', 'Meridian') AS division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid
+    substation_evw.substation_name,
+    getaway_evw.feederid
 FROM
     (
-             disgisdb.getaway_evw
-        INNER JOIN disgisdb.subunit_evw ON disgisdb.getaway_evw.subunit_globalid = disgisdb.subunit_evw.globalid
+             getaway_evw
+        INNER JOIN subunit_evw ON getaway_evw.subunit_globalid = subunit_evw.globalid
     )
-    INNER JOIN disgisdb.substation_evw ON disgisdb.subunit_evw.substation_globalid = disgisdb.substation_evw.globalid
+    INNER JOIN substation_evw ON subunit_evw.substation_globalid = substation_evw.globalid
 WHERE
-    disgisdb.getaway_evw.last_we = 'N'
+    getaway_evw.last_we = 'N'
 ORDER BY
     division,
-    disgisdb.substation_evw.substation_name,
-    disgisdb.getaway_evw.feederid
+    substation_evw.substation_name,
+    getaway_evw.feederid
