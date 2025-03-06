14d0fe69f2dcd1ca555fe40db78e5dfb1f87134d
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:34:39 -0600

    Update totalswitches.sql

--- Internship SQL Queries/SQL Queries (2024)/totalswitches.sql
+++ Internship SQL Queries/SQL Queries (2024)/totalswitches.sql
@@ -1,8 +1,8 @@
 select 
-    count(disgisdb.switch_evw.objectid) as count
+    count(switch_evw.objectid) as count
 from
-    disgisdb.switch_evw
+    switch_evw
 where
-    disgisdb.switch_evw.owned_by = 'MPC' 
-    and disgisdb.switch_evw.facility_status = 'E'
-    and disgisdb.switch_evw.feederid Not In ('KC','S2546','S2566','S2576')
+    switch_evw.owned_by = 'MPC' 
+    and switch_evw.facility_status = 'E'
+    and switch_evw.feederid Not In ('KC','S2546','S2566','S2576')
