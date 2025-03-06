b1f5722e44a01bfd0a8358a8de29f40c2f05498f
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:35:48 -0600

    Update unioncapreclsectswitfuse.sql

--- Internship SQL Queries/SQL Queries (2024)/unioncapreclsectswitfuse.sql
+++ Internship SQL Queries/SQL Queries (2024)/unioncapreclsectswitfuse.sql
@@ -1,10 +1,10 @@
-select disgisdb.capacitor_evw.loc_num as all_loc_num, disgisdb.capacitor_evw.feederid as all_feederid, disgisdb.capacitor_evw.objectid as all_objectid, 'Capacitor' as device from disgisdb.capacitor_evw
+select capacitor_evw.loc_num as all_loc_num, capacitor_evw.feederid as all_feederid, capacitor_evw.objectid as all_objectid, 'Capacitor' as device from capacitor_evw
 union all
-select disgisdb.recloser_evw.loc_num as all_loc_num, disgisdb.recloser_evw.feederid as all_feederid, disgisdb.recloser_evw.objectid as all_objectid, 'Recloser' as device from disgisdb.recloser_evw
+select recloser_evw.loc_num as all_loc_num, recloser_evw.feederid as all_feederid, recloser_evw.objectid as all_objectid, 'Recloser' as device from recloser_evw
 union all
-select disgisdb.sectionalizer_evw.loc_num as all_loc_num, disgisdb.sectionalizer_evw.feederid as all_feederid, disgisdb.sectionalizer_evw.objectid as all_objectid, 'Sectionalizer' as device from disgisdb.sectionalizer_evw
+select sectionalizer_evw.loc_num as all_loc_num, sectionalizer_evw.feederid as all_feederid, sectionalizer_evw.objectid as all_objectid, 'Sectionalizer' as device from sectionalizer_evw
 union all
-select disgisdb.switch_evw.loc_num as all_loc_num, disgisdb.switch_evw.feederid as all_feederid, disgisdb.switch_evw.objectid as all_objectid, 'Switch' as device from disgisdb.switch_evw
+select switch_evw.loc_num as all_loc_num, switch_evw.feederid as all_feederid, switch_evw.objectid as all_objectid, 'Switch' as device from switch_evw
 union all
-select disgisdb.fuse_evw.loc_num as all_loc_num, disgisdb.fuse_evw.feederid as all_feederid, disgisdb.fuse_evw.objectid as all_objectid, 'Fuse' as device from disgisdb.fuse_evw
+select fuse_evw.loc_num as all_loc_num, fuse_evw.feederid as all_feederid, fuse_evw.objectid as all_objectid, 'Fuse' as device from fuse_evw
 order by all_loc_num, device
