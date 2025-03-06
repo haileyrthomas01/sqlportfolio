b9f784eb9c7e4762daa84263c76e8cb27666b57a
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:17:25 -0600

    Update xfmrobjandbank.sql

--- Internship SQL Queries/SQL Queries (2023)/xfmrobjandbank.sql
+++ Internship SQL Queries/SQL Queries (2023)/xfmrobjandbank.sql
@@ -1,5 +1,5 @@
-select disgisdb.transformer_obj_evw.objectid, 
-(disgisdb.transformer_evw.mapx || ', ' || disgisdb.transformer_evw.mapy) as coordinates,
-disgisdb.transformer_evw.feederid, disgisdb.transformer_obj_evw.kva_rating as "SIZE", disgisdb.transformer_obj_evw.type
-from disgisdb.transformer_evw inner join disgisdb.transformer_obj_evw on
-    (disgisdb.transformer_evw.globalid = disgisdb.transformer_obj_evw.transformer_globalid)
+select transformer_obj_evw.objectid, 
+(transformer_evw.mapx || ', ' || transformer_evw.mapy) as coordinates,
+transformer_evw.feederid, transformer_obj_evw.kva_rating as "SIZE", transformer_obj_evw.type
+from transformer_evw inner join transformer_obj_evw on
+    (transformer_evw.globalid = transformer_obj_evw.transformer_globalid)
