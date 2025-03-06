dbc6dc12996e1732839fbf578c3f68bf385a7e53
Hailey Thomas <109697302+haileyrthomas01@users.noreply.github.com>
2025-03-06 15:30:44 -0600

    Update scadafeeders.sql

--- Internship SQL Queries/SQL Queries (2024)/scadafeeders.sql
+++ Internship SQL Queries/SQL Queries (2024)/scadafeeders.sql
@@ -1,9 +1,9 @@
 SELECT
-    count(disgisdb.getaway_evw.feederid) as "COUNT"
+    count(getaway_evw.feederid) as "COUNT"
 FROM
-    disgisdb.getaway_evw 
+    getaway_evw 
 WHERE   
-    disgisdb.getaway_evw.dagroup_globalid IS NOT NULL
-    and disgisdb.getaway_evw.last_we = 'N'
+    getaway_evw.dagroup_globalid IS NOT NULL
+    and getaway_evw.last_we = 'N'
 ORDER BY
     "COUNT"
